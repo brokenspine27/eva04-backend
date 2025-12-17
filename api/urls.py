a3b5e86ec27a53018aaa2ff33cd4e13f78b05ee4
brokenspine27 <doris.ilabaca@inacapmail.cl>
2025-12-17 17:46:43 -0300

    correcion error de urls

--- api/urls.py
+++ api/urls.py
@@ -6,13 +6,10 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('personas/', views.get_personas),
-    
-    path('crear/', views.crear_persona),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    path('persona/', views.persona_aleatoria, name='persona_aleatoria'),
-    path('personas/', views.get_personas, name='get_personas'),  # Ya e    path('all_persons/', views.todas_personas, name='all_persons'),
-    path('guardar_persona/', views.guardar_persona_api, name='guardar_persona'),   
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair')]
+    
+    path('persona/', views.persona_unica, name='persona_unica'),
+    path('personas/', views.get_personas, name='get_personas'),
+    path('guardar_persona/', views.guardar_persona, name='guardar_persona'),
+]
